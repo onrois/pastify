8b9121eda3c2f8dc3915488bab640ed8f4c02b8d
Hiren Rafaliya <hiren6997@gmail.com>
2024-09-07 15:01:14 +0530

    Initial Commit

---
+++ Pastify/PastifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PastifyApp.swift
+//  Pastify
+//
+//  Created by Hiren Rafaliya on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PastifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

fabb126cf933e8615d0e98a5643e33664d8ec16c
Hiren Rafaliya <hiren6997@gmail.com>
2024-09-07 22:16:58 +0530

    fix: category model

--- Pastify/Data/Local/Category.swift
+++ Pastify/Data/Local/Category.swift
@@ -9,11 +9,17 @@ import Foundation
 import SwiftUI
 
 struct Category {
+    let id: String
     let name: String
     let createdOn: Date
+    let order: Int
+    let color: Int
     
     init(_ name: String) {
+        self.id = UUID().uuidString
         self.name = name
         self.createdOn = Date.now
+        self.order = 1
+        self.color = Color.red.hashValue
     }
 }
